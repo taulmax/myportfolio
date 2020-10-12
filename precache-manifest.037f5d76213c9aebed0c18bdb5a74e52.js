64cb63b83976c079ab5368a1da421c37d10fc75a
Seo Jun Hyeok <46551990+taulmax@users.noreply.github.com>
2020-10-13 04:47:04 +0900

    Updates

--- precache-manifest.037f5d76213c9aebed0c18bdb5a74e52.js
+++ precache-manifest.037f5d76213c9aebed0c18bdb5a74e52.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "262711e19699e895dae42dedd2fbef28",
+    "revision": "55b1c4905995d6bf44d6a5b4b9bffd72",
     "url": "/myportfolio/index.html"
   },
   {
-    "revision": "3a4ba00b9745e36ed2d3",
+    "revision": "06b8b638d8f0706ede30",
     "url": "/myportfolio/static/css/main.fd6c8d01.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/myportfolio/static/js/2.2da97c97.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3a4ba00b9745e36ed2d3",
-    "url": "/myportfolio/static/js/main.4aa61c58.chunk.js"
+    "revision": "06b8b638d8f0706ede30",
+    "url": "/myportfolio/static/js/main.665b748b.chunk.js"
   },
   {
     "revision": "da2e02db1e870a8bb8e5",
