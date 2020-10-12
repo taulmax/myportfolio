40df98718cf68ccb62e71a8f6474e361c3d2193b
Seo Jun Hyeok <46551990+taulmax@users.noreply.github.com>
2020-10-13 03:07:48 +0900

    Updates

---
+++ precache-manifest.4228ac7a0666d408ca441edd12ae91d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "701e0028d91d03da9247c15831406dd2",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "452922e7e113c4c7035f",
+    "url": "/myportfolio/static/css/main.fd6c8d01.chunk.css"
+  },
+  {
+    "revision": "2aa5a29268fed183ffae",
+    "url": "/myportfolio/static/js/2.2da97c97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myportfolio/static/js/2.2da97c97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "452922e7e113c4c7035f",
+    "url": "/myportfolio/static/js/main.68278ba6.chunk.js"
+  },
+  {
+    "revision": "da2e02db1e870a8bb8e5",
+    "url": "/myportfolio/static/js/runtime-main.b5f0a273.js"
+  }
+]);
